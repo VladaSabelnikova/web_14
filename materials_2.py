b140dfbacec7b1acc387bb05296c757c87be3820
vlada <vladasabelnikova@gmail.com>
2022-03-20 19:43:26 +0300

    Убрала лишний app.run() в конце
    Всё

--- materials_2.py
+++ materials_2.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 import logging
 import json
@@ -176,4 +178,3 @@ def get_first_name(req):
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    app.run()
